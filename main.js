f754139aabb425d951cdfd9c1495ba6e19f377d4
DennisTarik <dm499555@gmail.com>
2021-10-12 17:28:59 +0200

    6 questions + end

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-const correctAnswer = true;
+const questionList = [
+    "Are you a Fish",
+    "Are you a Cat",
+    "Are you a Human",
+    "Are you a Dog",
+    "Are you a Camel",
+    "Are you a Bird",
+    "Are you a Cyborg"
+];
+
+let questionIndex = 0
+
+const answerList = [
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+]
+
+let correctAnswer = answerList[0]
 
 const myQuestion = document.querySelector(".question");
-myQuestion.textContent = "Bist du ein Mensch?";
+myQuestion.textContent = questionList[0];
 
 const yesButton = document.querySelector(".yes")
 yesButton.onclick = function () {
@@ -10,7 +32,6 @@ yesButton.onclick = function () {
     } else {
     showAnswerIsIncorrect()
     }
-    disableButtons();
 }; 
 const noButton = document.querySelector(".no")
 noButton.onclick = function () {
@@ -19,22 +40,38 @@ noButton.onclick = function () {
     } else {
     showAnswerIsIncorrect()
     }
-    disableButtons();
 }; 
-
 function showAnswerIsCorrect () {
     const resultElement = document.createElement("p");
     resultElement.textContent = "Deine Antwort ist richtig!";
     resultElement.className = "correct";
     document.body.appendChild(resultElement);
+    setTimeout(setNewQuestion, 500);
 }
 function showAnswerIsIncorrect () {
     const resultElement = document.createElement("p");
     resultElement.textContent = "Deine Antwort ist falsch!";
     resultElement.className = "incorrect";
     document.body.appendChild(resultElement);
+    disableButtons();
 }
 function disableButtons () {
     yesButton.disabled = true;
     noButton.disabled = true;
+}
+function setNewQuestion () {
+    questionIndex += 1;
+    myQuestion.textContent = questionList[questionIndex];
+    correctAnswer = answerList[questionIndex];
+    document.body.removeChild(document.querySelector(".correct"))
+    quizend();
+}
+function quizend () {
+    if (questionIndex === 7) {
+        document.body.removeChild(document.querySelector(".answer"));
+        const endElement = document.createElement("h1");
+        endElement.textContent = "Herzlichen Glückwunsch!!!";
+        endElement.className = "finish";
+        document.body.appendChild(endElement);
+    } else {}
 }
